eebbb19a53d8e3fa9eece723229ef3eef7e9a398
Wentana Ghile <wentanaghile@MacBook-Pro.local>
2023-07-27 13:15:05 -0400

    Get Even answered

--- Wk4Day3CW.js
+++ Wk4Day3CW.js
@@ -6,12 +6,12 @@
             console.log(i)
         }    
 
-    // //Get Even
-    //     for(let i = 0; i <= 200; i++){
-    //         if(i % 2 == 0){
-    //             console.log(i)
-    //         }
-    //     }
+    //Get Even
+        for(let i = 0; i <= 200; i++){
+            if(i % 2 == 0){
+                console.log(i)
+            }
+        }
 
     // //Fizz Buzz
     //     for(let i = 1; i <= 100; i++){
