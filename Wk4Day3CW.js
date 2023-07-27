912c2ffff8ed9f784255366b1611b69c84bfe61f
Wentana Ghile <wentanaghile@MacBook-Pro.local>
2023-07-27 13:15:49 -0400

    Fizz Buzz answered

--- Wk4Day3CW.js
+++ Wk4Day3CW.js
@@ -13,18 +13,18 @@
             }
         }
 
-    // //Fizz Buzz
-    //     for(let i = 1; i <= 100; i++){
-    //         if(i % 5 == 0 && i % 3 == 0){
-    //             console.log('FizzBuzz')
-    //         }else if(i % 5 == 0){
-    //             console.log('Buzz')
-    //         }else if(i % 3 == 0){
-    //             console.log('Fizz')
-    //         }else{
-    //             console.log(i)
-    //         }
-    //     }
+    //Fizz Buzz
+        for(let i = 1; i <= 100; i++){
+            if(i % 5 == 0 && i % 3 == 0){
+                console.log('FizzBuzz')
+            }else if(i % 5 == 0){
+                console.log('Buzz')
+            }else if(i % 3 == 0){
+                console.log('Fizz')
+            }else{
+                console.log(i)
+            }
+        }
 
     // //Wild Wild Life
     //     //array: [name, species, age, hometown]
