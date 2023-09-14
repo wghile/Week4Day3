6022940643a636e151571eaa66e9c753a5768e9c
Wentana Ghile <wentanaghile@MacBook-Pro.local>
2023-09-14 16:28:46 -0400

    added a ReadMe

--- Wk4Day3CW.js
+++ Wk4Day3CW.js
@@ -2,11 +2,13 @@
 
 //Part II: JavaScript Reps
     //Easy Going
+        //Write a for loop that will log the numbers 1 through 20
         for(let i = 1; i <= 20; i++){
             console.log(i)
         }    
 
     //Get Even
+        //Write a for loop that will log only the even numbers in 0 through 200.
         for(let i = 0; i <= 200; i++){
             if(i % 2 == 0){
                 console.log(i)
@@ -14,6 +16,10 @@
         }
 
     //Fizz Buzz
+        //Write a javascript application that logs all numbers from 1 - 100.
+            // If a number is divisible by 3 log "Fizz" instead of the number.
+            // If a number is divisible by 5 log "Buzz" instead of the number.
+            // If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
         for(let i = 1; i <= 100; i++){
             if(i % 5 == 0 && i % 3 == 0){
                 console.log('FizzBuzz')
